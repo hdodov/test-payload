0abba8f7c50425ee780ba1a9777e7437fa646a4f
Hristiyan Dodov <h.dodov@gmail.com>
2024-11-08 14:04:20 +0200

    Add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
+import initBundleAnalyzer from '@next/bundle-analyzer'
 import { withPayload } from '@payloadcms/next/withPayload'
+
+const withBundleAnalyzer = initBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
 }
 
-export default withPayload(nextConfig)
+export default withBundleAnalyzer(withPayload(nextConfig))
