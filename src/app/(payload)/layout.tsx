380b85b1dcc367ba05a9dcf1f23258b017cc7c7f
Hristiyan Dodov <h.dodov@gmail.com>
2024-11-27 17:59:43 +0200

    Upgrade Payload to 3.2.1

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,19 +1,30 @@
+// https://github.com/payloadcms/payload/releases/tag/v3.0.0-beta.128
+
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
-import configPromise from '@payload-config'
+import config from '@payload-config'
 import '@payloadcms/next/css'
-import { RootLayout } from '@payloadcms/next/layouts'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import type { ServerFunctionClient } from 'payload'
 import React from 'react'
-
-import './custom.scss'
 import { importMap } from './admin/importMap'
+import './custom.scss'
 
 type Args = {
   children: React.ReactNode
 }
 
+const serverFunction: ServerFunctionClient = async function (args: any) {
+  'use server'
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
 const Layout = ({ children }: Args) => (
-  <RootLayout importMap={importMap} config={configPromise}>
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 )
